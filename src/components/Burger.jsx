19289efc4af96710440ea65b21086af0e13ce58d
Timothy Zirngibl <36170481+TZirngibl@users.noreply.github.com>
2021-05-28 12:01:01 -0400

    Updates

--- src/components/Burger.jsx
+++
@@ -1,9 +0,0 @@
-const Burger = () => {
-    return (
-        <div className="mobile-toggle md:hidden z-50 transform rotate-90 text-4xl absolute right-8">
-            III
-        </div>
-    )
-}
-
-export default Burger
